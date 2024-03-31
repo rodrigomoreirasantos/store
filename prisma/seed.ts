481d59cf598d43a817a5efa740dbaa9ea826d491
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-03-31 15:21:57 -0400

    feat: change image urls for mouses and keyboards

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -20,10 +20,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
+          "https://utfs.io/f/c8ca4858-7f0f-433e-9cc8-936e2f1a151c-otetkp.png",
+          "https://utfs.io/f/24bbf4b9-3ba2-4d17-9291-c8f2eb1ead92-x6ep3u.png",
+          "https://utfs.io/f/ba717d9a-47e4-46ef-a353-5064e486c46d-thphc5.png",
+          "https://utfs.io/f/bcf27822-8ceb-41d1-8ce5-c770cf852536-l4plt0.png",
         ],
         basePrice: 650,
         categoryId: mousesCategory.id,
@@ -36,9 +36,9 @@ async function main() {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
           "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
+          "https://utfs.io/f/7b305da2-084e-4924-b56f-c73a7afde48e-40f9t2.png",
+          "https://utfs.io/f/077e022e-95c1-4446-a676-dce9cee83103-qe5e93.png",
+          "https://utfs.io/f/4a122c8b-11ea-4b43-84e9-8dfc15bb05fb-m98ja0.png",
         ],
         basePrice: 750,
         categoryId: mousesCategory.id,
@@ -50,10 +50,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
+          "https://utfs.io/f/763bf207-2dfb-4aa4-a633-61718a2c4d51-6pfvtn.png",
+          "https://utfs.io/f/c108e7d0-da91-4db7-a6ae-0dec03a8db4c-t3609o.png",
+          "https://utfs.io/f/2855b69d-345a-40c0-8f0f-65a6b62f7cbb-jk7x9f.png",
+          "https://utfs.io/f/0a100cb9-6208-4f25-85d5-20dd83a3cf7d-2ti76m.png",
         ],
         basePrice: 300,
         categoryId: mousesCategory.id,
@@ -65,10 +65,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
+          "https://utfs.io/f/12eaba65-1d8c-4d57-a9e6-52dfb1e74697-1xd5uq.png",
+          "https://utfs.io/f/20f33895-c218-4fbb-ba96-a25456dece06-7s04kc.png",
           "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
+          "https://utfs.io/f/f649d3b3-d8d5-4ae0-b907-555b2c8a1d58-cmorrv.png",
         ],
         basePrice: 600,
         categoryId: mousesCategory.id,
@@ -80,10 +80,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
-          "https://utfs.io/f/ba2c005c-6a54-400b-8fe0-efb49aea5201-hejlql.png",
+          "https://utfs.io/f/78894238-3978-41f1-aca4-b4d212781462-5jk68j.png",
+          "https://utfs.io/f/a5dc98d8-e3c9-458b-97a4-67594e735667-m16w6s.png",
+          "https://utfs.io/f/234fd685-40fa-43d8-b394-94a6b0e1ecb1-wiafu3.png",
+          "https://utfs.io/f/da2d1a28-1799-41b4-902a-22d001c88f19-g0npvu.png",
         ],
         basePrice: 350,
         categoryId: mousesCategory.id,
@@ -112,9 +112,9 @@ async function main() {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
           "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/6010aa47-0155-40bb-9b37-e859d7b8aeb0-ausijr.png",
+          "https://utfs.io/f/0c421d22-002c-4907-afe6-582d207493be-otkh7q.png",
+          "hhttps://utfs.io/f/8f959fce-38e0-4cb0-b335-219bcc6d495e-w8rm3f.png",
         ],
         basePrice: 650,
         categoryId: keyboardsCategory.id,
@@ -126,10 +126,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/dfb0e9cc-749a-4609-9b0f-02837529ddb3-m9yl8y.png",
+          "https://utfs.io/f/ae0dfb13-a9b8-4806-86d4-4d6911ca9f96-du02k1.png",
+          "https://utfs.io/f/5f6cd025-3127-48b2-80ce-84234e4c95d3-5e1jv4.png",
+          "https://utfs.io/f/f1231a8e-25c2-4511-b0e2-a2883b08f479-31wytt.png",
         ],
         basePrice: 750,
         categoryId: keyboardsCategory.id,
@@ -141,10 +141,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/427049c3-9b77-4968-8de6-253d9fc2ba30-u67teu.png",
+          "https://utfs.io/f/be05ad50-63db-489a-a19e-42d474b8227a-jowbfg.png",
+          "https://utfs.io/f/be05ad50-63db-489a-a19e-42d474b8227a-jowbfg.png",
+          "https://utfs.io/f/da64efca-b399-49e4-aa54-b1d98b2257d6-qeno4j.png",
         ],
         basePrice: 440,
         categoryId: keyboardsCategory.id,
@@ -156,10 +156,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/848f0842-5345-4320-acaf-c693d3680038-tbfuuf.png",
+          "https://utfs.io/f/f915b150-6978-44fb-95a5-5aa301948b44-mk1c1y.png",
+          "https://utfs.io/f/3079d117-7f5e-441d-891c-b81f1ef67d62-fsmt9h.png",
+          "https://utfs.io/f/7c6635a3-588c-41af-ac2b-75859401401f-918ah0.png",
         ],
         basePrice: 700,
         categoryId: keyboardsCategory.id,
@@ -171,10 +171,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
+          "https://utfs.io/f/6bcfd697-047f-4893-9aa2-5e0c6767789e-4b9e6c.png",
           "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/3e5105c0-1425-4f24-8ff3-13957641d100-kmao8t.png",
+          "https://utfs.io/f/d5ad0728-37ae-4731-8488-00d4ac158b7f-ph9bb6.png",
         ],
         basePrice: 500,
         categoryId: keyboardsCategory.id,
@@ -186,10 +186,10 @@ async function main() {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id malesuada elit, eget vulputate justo. Sed sollicitudin velit dolor, ut gravida odio iaculis a.\nNulla risus justo, tempor eu felis eu, efficitur pulvinar risus. Sed viverra, nisi id egestas convallis, massa lorem convallis magna, vel convallis mi turpis nec ante. Maecenas posuere lacus id gravida dignissim. Morbi sit amet rutrum ex. Duis sit amet sem orci. Morbi non nisl sed mauris mattis ullamcorper quis eget metus.\nUt pellentesque ornare erat, vitae blandit ex pulvinar sit amet. Ut pellentesque lorem at eros vestibulum lobortis. Proin bibendum est facilisis nulla tristique vestibulum. Etiam placerat tortor sit amet lacinia volutpat. Curabitur lectus turpis, faucibus vitae tortor in, lacinia tristique neque.",
         imageUrls: [
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
-          "https://utfs.io/f/cef25187-51d4-4fb8-a7c7-92474d8fc07e-y9wlrz.png",
+          "https://utfs.io/f/fde09309-9b0e-4a29-b2e3-6a635997fb13-b14pyz.png",
+          "https://utfs.io/f/df3ce3f0-cdc6-4015-b0c4-23cd55f7e612-7jaqka.png",
+          "https://utfs.io/f/084f0557-dd16-4ae6-9026-52ebf73c85a5-41gr5l.png",
+          "https://utfs.io/f/97aa9d7c-4c54-4b20-8c42-2d7fb4b5ef43-jmrqw.png",
         ],
         basePrice: 400,
         categoryId: keyboardsCategory.id,
