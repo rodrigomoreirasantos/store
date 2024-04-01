0100179b8fe28b10b461f0506bc8042569995b47
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-01 10:48:41 -0400

    feat: ensure recommended products re shown on product detail page and change imports

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { prismaClient } from "@/lib/prisma";
-import ProductImages from "../components/product-image";
-import ProductInfo from "../components/product-info";
+import ProductImages from "./components/product-image";
+import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/product";
+import ProductList from "@/components/ui/product-list";
+import SectionTitle from "@/components/ui/section-title";
 
 interface ProductDetailPageProp {
   params: {
@@ -16,14 +18,31 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
-    <div>
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+      <div className="flex flex-col gap-5">
+        <SectionTitle className="pl-5">Produtos Recomendados</SectionTitle>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   );
 };
