eb505cd374d08c459d374a341669258e5efb647c
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-01 11:00:24 -0400

    feat: add DiscountBadge component

--- components/ui/product-item.tsx
+++ components/ui/product-item.tsx
@@ -1,9 +1,8 @@
-import { Product } from "@prisma/client";
 import Image from "next/image";
-import { Badge } from "./badge";
-import { ArrowDownIcon } from "lucide-react";
+
 import { ProductWithTotalPrice } from "@/helpers/product";
 import Link from "next/link";
+import DiscountBadge from "./discount-badge";
 
 interface ProductItemProp {
   product: ProductWithTotalPrice;
@@ -27,9 +26,9 @@ const ProductItem = ({ product }: ProductItemProp) => {
           />
 
           {product.discountPercentage > 0 && (
-            <Badge className="absolute left-3 top-3 px-2 py-[2px] ">
-              <ArrowDownIcon size={14} /> {product.discountPercentage}%
-            </Badge>
+            <DiscountBadge className="absolute left-3 top-3">
+              {product.discountPercentage}
+            </DiscountBadge>
           )}
         </div>
 
