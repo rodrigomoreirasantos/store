1cb64b95f4884c5e6d4c02ec17b418fc1bddbf27
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-06 13:27:08 -0400

    feat: add HOST_URL

--- actions/checkout.ts
+++ actions/checkout.ts
@@ -14,8 +14,8 @@ export const createCheckout = async (
   const checkout = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
-    success_url: "http://localhost:3000",
-    cancel_url: "http://localhost:3000",
+    success_url: process.env.HOST_URL,
+    cancel_url: process.env.HOST_URL,
     metadata: {
       // orderId,
       products: JSON.stringify(products),
