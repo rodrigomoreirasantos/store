7577f3964fc722483dfb9ed4e68b041be1b0b5ba
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-06 12:34:20 -0400

    feat: add stripe checkout to cart product

---
+++ actions/checkout.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { CartProduct } from "@/providers/cart";
+import Stripe from "stripe";
+
+export const createCheckout = async (
+  products: CartProduct[],
+  // orderId: string,
+) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2023-10-16",
+  });
+
+  const checkout = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: "http://localhost:3000",
+    cancel_url: "http://localhost:3000",
+    metadata: {
+      // orderId,
+    },
+    line_items: products.map((product) => {
+      return {
+        price_data: {
+          currency: "cad",
+          product_data: {
+            name: product.name,
+            description: product.description,
+            images: product.imageUrls,
+          },
+          unit_amount: Number(product.totalPrice) * 100,
+        },
+        quantity: product.quantity,
+      };
+    }),
+  });
+
+  return checkout;
+};
--- components/ui/cart.tsx
+++ components/ui/cart.tsx
@@ -7,11 +7,21 @@ import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { Button } from "./button";
 import { ScrollArea } from "./scroll-area";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const { products, total, subTotal, totalDiscount } = useContext(CartContext);
 
-  const handleFinishPurchaseClick = async () => {};
+  const handleFinishPurchaseClick = async () => {
+    const checkout = await createCheckout(products);
+
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({
+      sessionId: checkout.id,
+    });
+  };
 
   return (
     <div className="flex h-full flex-col gap-5">
