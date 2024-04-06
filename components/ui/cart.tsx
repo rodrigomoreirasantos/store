60b982d87421febc6735785e433d476976f15186
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-06 12:09:16 -0400

    feat: ensure cart products can be scroll

--- components/ui/cart.tsx
+++ components/ui/cart.tsx
@@ -6,6 +6,7 @@ import CartItem from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { Button } from "./button";
+import { ScrollArea } from "./scroll-area";
 
 const Cart = () => {
   const { products, total, subTotal, totalDiscount } = useContext(CartContext);
@@ -13,7 +14,7 @@ const Cart = () => {
   const handleFinishPurchaseClick = async () => {};
 
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex h-full flex-col gap-5">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -22,19 +23,23 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductTotalPrice(product as any) as any}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Carrinho vazio. Vamos fazer compras?
-          </p>
-        )}
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       {products.length > 0 && (
