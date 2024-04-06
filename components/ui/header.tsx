fe241cbc7eb05afa1c2a57dec9ce35f81beb2a7e
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-06 14:04:52 -0400

    feat: add persistense to cart product

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -93,11 +93,17 @@ const Header = () => {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Início
-            </Button>
-
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} />
+                  Home
+                </Button>
+              </Link>
+            </SheetClose>
             <Button variant="outline" className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
