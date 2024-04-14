f353355e0aba19c40f373e1bd66586c05677eef4
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-14 12:15:27 -0400

    feat:  ensure product detail page is responsive on desktop

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -36,11 +36,14 @@ const ProductDetailsPage = async ({
   if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8 pb-8">
-      <ProductImages imageUrls={product.imageUrls} name={product.name} />
-      <ProductInfo product={computeProductTotalPrice(product)} />
+    <div className="flex flex-col gap-8 pb-8 lg:container lg:mx-auto lg:gap-10 lg:py-10">
+      <div className="flex flex-col gap-8 lg:flex-row lg:gap-9  lg:px-5">
+        <ProductImages imageUrls={product.imageUrls} name={product.name} />
+        <ProductInfo product={computeProductTotalPrice(product)} />
+      </div>
+
       <div className="flex flex-col gap-5">
-        <SectionTitle>Productos Recomendados</SectionTitle>
+        <SectionTitle className="pl-5">Productos Recomendados</SectionTitle>
         <ProductList products={product.category.products} />
       </div>
     </div>
