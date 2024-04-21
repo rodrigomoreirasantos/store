5fbbfb03fd3e74690f690099cfaa2b1d554d6b80
rodrigo <rodrigomoreirasantos01@gmail.com>
2024-04-21 19:20:54 -0400

    feat: ensure products are fetched on products page

---
+++ app/(admin)/dashboard/products/page.tsx
@@ -0,0 +1,8 @@
+import { prismaClient } from "@/lib/prisma";
+
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany();
+  return <h1>Product</h1>;
+};
+
+export default ProductsPage;
