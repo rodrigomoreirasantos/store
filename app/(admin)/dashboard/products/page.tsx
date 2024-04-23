bbf5392172f9c88b616e99a5133e180c46fc4a9a
Rodrigo Moreira Santos <rodrigomoreirasantos01@gmail.com>
2024-04-23 10:43:55 -0400

    feat: ensure product category is shown on product table

--- app/(admin)/dashboard/products/page.tsx
+++ app/(admin)/dashboard/products/page.tsx
@@ -3,14 +3,26 @@ import { Button } from "@/components/ui/button";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 import { PackageIcon, PlusIcon } from "lucide-react";
-import ProductsTable from "./components/product-table";
+import ProductsTable, {
+  ProductWithTotalPriceAndCategory,
+} from "./components/product-table";
 
 const ProductsPage = async () => {
-  const products = await prismaClient.product.findMany();
+  const products = await prismaClient.product.findMany({
+    include: {
+      category: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
 
-  const productsWithTotalPrice = products.map((product) =>
-    computeProductTotalPrice(product),
-  );
+  const productsWithTotalPrice: ProductWithTotalPriceAndCategory[] =
+    products.map((product) => ({
+      ...computeProductTotalPrice(product),
+      category: product.category,
+    }));
 
   return (
     <div className="flex w-full flex-col gap-10 p-10">
